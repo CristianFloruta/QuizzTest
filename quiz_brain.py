5c0fc558437840fcc06e3c150e0db24c066144f1
Cristian FLORUTA <florutacristian@gmail.com>
2022-12-26 19:12:49 +0200

    Add QuizzBrain class methods explanation

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,17 +1,27 @@
 class QuizzBrain:
 
+    # QuizzBrain is creating the game logic for question and answer game
+
     def __init__(self, question_list):
         self.question_num = 0
         self.question_list = question_list
         self.score = 0
 
     def still_has_questions(self):
+
+        # setting the stop for game running loop
+
         if self.question_num < len(self.question_list):
             return True
         else:
             return False
 
     def next_question(self):
+
+        """Ask and verify the answer for a question from the class list.
+        Count the questions asked.
+        Break the game loop when 'Stop' is chosen """
+
         question = self.question_list[self.question_num]
         self.question_num += 1
         answer = input(f"Q.{self.question_num}: {question.text} (True/False): ").capitalize()
